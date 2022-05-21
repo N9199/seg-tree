f8d35ddf0087c4767a901dbb060f6319d844e6af
N9199 <N9199@users.noreply.github.com>
2022-05-20 21:10:59 -0400

    Small documentation fix

--- src/segment_tree/persistent_lazy.rs
+++ src/segment_tree/persistent_lazy.rs
@@ -45,7 +45,7 @@ where
     /// Returns the result from the range `[left,right]` from the version of the segment tree.
     /// It returns None if and only if range is empty.
     /// It will **panic** if left or right are not in [0,n), or if version is not in [0,[versions](LazyPersistentSegmentTree::versions)).
-    /// It has time complexity of `O(log(n))`, assuming that [combine](Node::combine), [update_lazy_value](LazyNode::update_lazy_value) and [update_lazy_value](LazyNode::lazy_update) have constant time complexity.
+    /// It has time complexity of `O(log(n))`, assuming that [combine](Node::combine), [update_lazy_value](LazyNode::update_lazy_value) and [lazy_update](LazyNode::lazy_update) have constant time complexity.
     pub fn query(&mut self, version: usize, left: usize, right: usize) -> Option<T> {
         self.query_helper(self.roots[version], left, right, 0, self.n - 1)
     }
@@ -99,7 +99,7 @@ where
 
     /// Creates a new segment tree version from version were the p-th element of the segment tree to value T and update the segment tree correspondingly.
     /// It will panic if p is not in `[0,n)`, or if version is not in [0,[versions](LazyPersistentSegmentTree::versions)).
-    /// It has time complexity of `O(log(n))`, assuming that [combine](Node::combine), [update_lazy_value](LazyNode::update_lazy_value) and [update_lazy_value](LazyNode::lazy_update) have constant time complexity.
+    /// It has time complexity of `O(log(n))`, assuming that [combine](Node::combine), [update_lazy_value](LazyNode::update_lazy_value) and [lazy_update](LazyNode::lazy_update) have constant time complexity.
     pub fn update(&mut self, version: usize, left: usize, right: usize, value: <T as Node>::Value) {
         let new_root = self.update_helper(self.roots[version], left, right, &value, 0, self.n - 1);
         self.roots.push(new_root);
