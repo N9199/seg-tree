8c1ab0b47088141748660222f6f307e29c323bc7
N9199 <N9199@users.noreply.github.com>
2022-07-14 17:18:46 -0400

    Add LazySetWrapper and refactor correspondingly, fix empty slice build underflow

--- src/nodes/node.rs
+++ src/nodes/node.rs
@@ -1,7 +1,7 @@
 /// Base trait required by nodes of segment trees. A type which implements this trait is essentially a "compressed" representation of a non-empty segment of [values](Node::Value). 
 pub trait Node {
     /// This type corresponds to the type of the information to create the node with [Node::initialize].
-    type Value;
+    type Value: Clone;
     /// Function to create nodes from saved value, it is assumed that even if there's more data saved in the node, `value` should have enough data to create **all** of the data of a node of a segment segment of exactly one element.
     fn initialize(value: &Self::Value) -> Self;
     /// Function which will combine nodes `a` and `b`, where each corresponds to segments `[i,j]` and `[j+1,k]` respectively, into a node which corresponds to the segment `[i,k]`. This function **must** be associative (taking \* as a symbol for combine, we have that a\*(b\*c)==(a\*b)\*c is true), but need not be commutative (it's not necessarily true that a\*b==b\*a).
--- src/utils/min.rs
+++ src/utils/min.rs
@@ -1,14 +1,13 @@
-use crate::nodes::{LazyNode, Node};
+use crate::nodes::Node;
 
-/// Implementation of range min for generic type T, it implements [Node] and [LazyNode].
+/// Implementation of range min for generic type T, it only implements [Node].
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Min<T>
-where
-    T: Ord + Clone,
 {
     value: T,
-    lazy_value: Option<T>,
 }
+
+
 
 impl<T> Node for Min<T>
 where
@@ -16,48 +15,20 @@ where
 {
     type Value = T;
     fn initialize(v: &Self::Value) -> Self {
-        Min {
-            value: v.clone(),
-            lazy_value: None,
-        }
+        Min { value: v.clone() }
     }
     fn combine(a: &Self, b: &Self) -> Self {
         Min {
             value: a.value.clone().min(b.value.clone()),
-            lazy_value: None,
         }
     }
     fn value(&self) -> &Self::Value {
         &self.value
     }
 }
-
-/// Implementation for minimum range query, the update sets each item in the range to the given value.
-impl<T> LazyNode for Min<T>
-where
-    T: Ord + Clone,
-{
-    fn lazy_update(&mut self, _i: usize, _j: usize) {
-        if let Some(value) = self.lazy_value.take() {
-            self.value = value
-        }
-    }
-
-    fn update_lazy_value(&mut self, v: &<Self as Node>::Value) {
-        self.lazy_value = Some(v.clone());
-    }
-
-    fn lazy_value(&self) -> Option<&<Self as Node>::Value> {
-        self.lazy_value.as_ref()
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::{
-        nodes::{LazyNode, Node},
-        utils::Min,
-    };
+    use crate::{nodes::Node, utils::Min};
 
     #[test]
     fn min_works() {
@@ -66,21 +37,5 @@ mod tests {
             .iter()
             .fold(Min::initialize(&0), |acc, new| Min::combine(&acc, new));
         assert_eq!(result.value(), &0);
-    }
-
-    #[test]
-    fn update_lazy_value_works() {
-        let mut node = Min::initialize(&1);
-        node.update_lazy_value(&2);
-        assert_eq!(node.lazy_value(), Some(&2));
-    }
-
-    #[test]
-    fn lazy_update_works() {
-        // Node represents the range [0,10] with min 1.
-        let mut node = Min::initialize(&1);
-        node.update_lazy_value(&2);
-        node.lazy_update(0, 10);
-        assert_eq!(node.value(), &2);
     }
 }
