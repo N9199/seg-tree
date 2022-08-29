2f4a391ad5153bd1ea409fa78efeb635cf8f3fc6
N9199 <N9199@users.noreply.github.com>
2022-08-29 17:10:43 -0400

    Add more benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -5,52 +5,40 @@ use std::time::Duration;
 use criterion::{
     black_box, criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput,
 };
-use rand::{distributions::Uniform, prelude::Distribution};
-use seg_tree::{nodes::Node, *};
-mod iterative {
-    use seg_tree::nodes::Node;
+use rand::{distributions::Uniform, prelude::Distribution, Rng};
+use seg_tree::{
+    nodes::Node,
+    utils::{LazySetWrapper, Min},
+    *,
+};
 
-    #[derive(Clone, Debug, Eq, PartialEq)]
-    pub struct Min<T>
-    where
-        T: Ord + Clone,
-    {
-        value: T,
-    }
+type LSMin<T> = LazySetWrapper<Min<T>>;
+const N: i64 = 1_000_000;
 
-    impl<T> Node for Min<T>
-    where
-        T: Ord + Clone,
-    {
-        type Value = T;
-        fn initialize(v: &Self::Value) -> Self {
-            Min { value: v.clone() }
-        }
-        fn combine(a: &Self, b: &Self) -> Self {
-            Min {
-                value: a.value.clone().min(b.value.clone()),
-            }
-        }
-        fn value(&self) -> &Self::Value {
-            &self.value
-        }
-    }
-}
-pub fn segment_tree_queries_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("segment_tree_queries_benchmark");
+pub fn recursive_segment_tree_queries_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("recursive_segment_tree_queries_benchmark");
+    let mut rng = rand::thread_rng();
+    let node_distr = Uniform::from(-N..=N);
     for i in 1..=6 {
         for j in 1..10 {
             let n = j * 10_usize.pow(i);
-            let nodes: Vec<_> = (0..=n).map(|x| iterative::Min::initialize(&x)).collect();
-            let segment_tree = Iterative::build(&nodes);
-            let distr = Uniform::from(0..n);
-            let mut rng = rand::thread_rng();
+            let nodes: Vec<_> = (&mut rng)
+                .sample_iter(node_distr)
+                .map(|x| Min::initialize(&x))
+                .take(n)
+                .collect();
+            let segment_tree = Recursive::build(&nodes);
+            let index_distr = Uniform::from(0..n);
             group.throughput(Throughput::Elements(n as u64));
             group.warm_up_time(Duration::from_secs(1));
             group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
                 b.iter_batched(
-                    || distr.sample(&mut rng),
-                    |mut i| segment_tree.query(i, i),
+                    || {
+                        Some((index_distr.sample(&mut rng), index_distr.sample(&mut rng)))
+                            .map(|(i, j)| (i.min(j), i.max(j)))
+                            .unwrap()
+                    },
+                    |(i, j)| segment_tree.query(i, j),
                     BatchSize::SmallInput,
                 );
             });
@@ -59,5 +47,165 @@ pub fn segment_tree_queries_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, segment_tree_queries_benchmark);
+pub fn lazy_recursive_segment_tree_queries_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("lazy_recursive_segment_tree_queries_benchmark");
+    let mut rng = rand::thread_rng();
+    let node_distr = Uniform::from(-N..=N);
+    for i in 1..=6 {
+        for j in 1..10 {
+            let n = j * 10_usize.pow(i);
+            let nodes: Vec<_> = (&mut rng)
+                .sample_iter(node_distr)
+                .map(|x| LSMin::initialize(&x))
+                .take(n)
+                .collect();
+            let mut segment_tree = LazyRecursive::build(&nodes);
+            let index_distr = Uniform::from(0..n);
+            group.throughput(Throughput::Elements(n as u64));
+            group.warm_up_time(Duration::from_secs(1));
+            group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
+                b.iter_batched(
+                    || {
+                        Some((index_distr.sample(&mut rng), index_distr.sample(&mut rng)))
+                            .map(|(i, j)| (i.min(j), i.max(j)))
+                            .unwrap()
+                    },
+                    |(i, j)| segment_tree.query(i, j),
+                    BatchSize::SmallInput,
+                );
+            });
+        }
+    }
+    group.finish();
+}
+pub fn iterative_segment_tree_queries_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("iterative_segment_tree_queries_benchmark");
+    let mut rng = rand::thread_rng();
+    let node_distr = Uniform::from(-N..=N);
+    for i in 1..=6 {
+        for j in 1..10 {
+            let n = j * 10_usize.pow(i);
+            let nodes: Vec<_> = (&mut rng)
+                .sample_iter(node_distr)
+                .map(|x| Min::initialize(&x))
+                .take(n)
+                .collect();
+            let segment_tree = Iterative::build(&nodes);
+            let index_distr = Uniform::from(0..n);
+            group.throughput(Throughput::Elements(n as u64));
+            group.warm_up_time(Duration::from_secs(1));
+            group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
+                b.iter_batched(
+                    || {
+                        Some((index_distr.sample(&mut rng), index_distr.sample(&mut rng)))
+                            .map(|(i, j)| (i.min(j), i.max(j)))
+                            .unwrap()
+                    },
+                    |(i, j)| segment_tree.query(i, j),
+                    BatchSize::SmallInput,
+                );
+            });
+        }
+    }
+    group.finish();
+}
+
+pub fn iterative_segment_tree_updates_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("iterative_segment_tree_updates_benchmark");
+    let mut rng = rand::thread_rng();
+    let node_distr = Uniform::from(-N..=N);
+    for i in 1..=6 {
+        for j in 1..10 {
+            let n = j * 10_usize.pow(i);
+            let nodes: Vec<_> = (&mut rng)
+                .sample_iter(node_distr)
+                .map(|x| Min::initialize(&x))
+                .take(n)
+                .collect();
+            let mut segment_tree = Iterative::build(&nodes);
+            let index_distr = Uniform::from(0..n);
+            group.throughput(Throughput::Elements(n as u64));
+            group.warm_up_time(Duration::from_secs(1));
+            group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
+                b.iter_batched(
+                    || (index_distr.sample(&mut rng), node_distr.sample(&mut rng)),
+                    |(i, v)| segment_tree.update(i, &v),
+                    BatchSize::SmallInput,
+                );
+            });
+        }
+    }
+    group.finish();
+}
+pub fn recursive_segment_tree_updates_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("iterative_segment_tree_updates_benchmark");
+    let mut rng = rand::thread_rng();
+    let node_distr = Uniform::from(-N..=N);
+    for i in 1..=6 {
+        for j in 1..10 {
+            let n = j * 10_usize.pow(i);
+            let nodes: Vec<_> = (&mut rng)
+                .sample_iter(node_distr)
+                .map(|x| Min::initialize(&x))
+                .take(n)
+                .collect();
+            let mut segment_tree = Recursive::build(&nodes);
+            let index_distr = Uniform::from(0..n);
+            group.throughput(Throughput::Elements(n as u64));
+            group.warm_up_time(Duration::from_secs(1));
+            group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
+                b.iter_batched(
+                    || (index_distr.sample(&mut rng), node_distr.sample(&mut rng)),
+                    |(i, v)| segment_tree.update(i, &v),
+                    BatchSize::SmallInput,
+                );
+            });
+        }
+    }
+    group.finish();
+}
+pub fn lazy_recursive_segment_tree_updates_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("iterative_segment_tree_updates_benchmark");
+    let mut rng = rand::thread_rng();
+    let node_distr = Uniform::from(-N..=N);
+    for i in 1..=6 {
+        for j in 1..10 {
+            let n = j * 10_usize.pow(i);
+            let nodes: Vec<_> = (&mut rng)
+                .sample_iter(node_distr)
+                .map(|x| LSMin::initialize(&x))
+                .take(n)
+                .collect();
+            let mut segment_tree = LazyRecursive::build(&nodes);
+            let index_distr = Uniform::from(0..n);
+            group.throughput(Throughput::Elements(n as u64));
+            group.warm_up_time(Duration::from_secs(1));
+            group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
+                b.iter_batched(
+                    || {
+                        Some((
+                            index_distr.sample(&mut rng),
+                            index_distr.sample(&mut rng),
+                            node_distr.sample(&mut rng),
+                        ))
+                        .map(|(i, j, v)| (i.min(j), i.max(j), v))
+                        .unwrap()
+                    },
+                    |(i, j, v)| segment_tree.update(i, j, &v),
+                    BatchSize::SmallInput,
+                );
+            });
+        }
+    }
+    group.finish();
+}
+
+criterion_group!(
+    benches,
+    recursive_segment_tree_queries_benchmark,
+    iterative_segment_tree_queries_benchmark,
+    lazy_recursive_segment_tree_queries_benchmark,
+    recursive_segment_tree_updates_benchmark,
+    iterative_segment_tree_updates_benchmark
+);
 criterion_main!(benches);
